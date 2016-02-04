4c0169ba5c7e1a9e4bbde7da90d2ece3163cb9d6
sophieeverett <sophieeverett@Sophies-MacBook-Air-5.local>
2016-02-05 09:17:25 +1100

    With arrow animation

--- js/index.js
+++ js/index.js
@@ -1,14 +1,29 @@
 $(document).ready(function(){
-	$('#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	});
-
 $("#nav-icon4").click(function(){
+	$(this).toggleClass('open');
 	$("#nav-icon4 li").toggleClass("menuAnimate");
+	$("#nav-icon4 li").toggleClass("menuAnimate2");
+	$(".menu_box").toggleClass("menu_box_animate");
+	$("#menuClick").toggleClass("menuAnimate3");
 });
 
-$("#nav-icon4").click(function(){
+$("#menuClick").click(function(){
 	$(".menu_box").toggleClass("menu_box_animate");
+	$("#nav-icon4 li").toggleClass("menuAnimate");
+	$("#nav-icon4").toggleClass("open");
+	$("#nav-icon4 li").toggleClass("menuAnimate2");
+	$("#menuClick").toggleClass("menuAnimate3");
+
 });
+
+$(window).scroll( function(){
+  var topWindow = $(window).scrollTop();
+	var topWindow = topWindow * 1.5;
+  var windowHeight = $(window).height();
+  var position = topWindow / windowHeight;
+  position = 1 - position;
+  $('.arrow-wrap').css('opacity', position);
+});
+
 
 });
