a1d5556f6bdc494c8c70b7e018021b76d9d00dd4
sophieeverett <sophieeverett@Sophies-MacBook-Air-5.local>
2016-02-04 18:15:16 +1100

    Update

--- js/index.js
+++ js/index.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
 	$('#nav-icon4').click(function(){
 		$(this).toggleClass('open');
 	});
+
+$("#nav-icon4").click(function(){
+	$("#nav-icon4 li").toggleClass("menuAnimate");
+});
+
+$("#nav-icon4").click(function(){
+	$(".menu_box").toggleClass("menu_box_animate");
+});
+
 });
